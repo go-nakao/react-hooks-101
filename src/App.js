4eaa7858e71a2cfeea12ee22d037ab52ea81661b
go-nakao <79854138+go-nakao@users.noreply.github.com>
2022-09-15 21:10:44 +0900

    カウントボタン実装

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const App = () => {
+  const [count, setCount] = useState(0);
+
+  //イベント処理(直前の状態を引数として、関数を渡す)
+  const countUp = () => {
+    setCount((count) => count + 1);
+  }
+  const countDown = () => {
+    setCount((count) => count - 1);
+  }
+
   return (
-    <div>
-      This is a templete for react.
-    </div>
+    <>
+      <p>カウント:{count}</p>
+      <button onClick={countUp}>カウントアップ</button>
+      <br />
+      <br />
+      <button onClick={countDown}>カウントダウン</button>
+    </>
   );
 }
