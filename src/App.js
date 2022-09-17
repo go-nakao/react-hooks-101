9d92f00adaf263fa38f244bea01580a850ccce5e
go-nakao <79854138+go-nakao@users.noreply.github.com>
2022-09-17 12:29:20 +0900

    propsの内容を定義

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { useState } from "react";
 
-export const App = () => {
-  //初期値の設定
+export const App = props => {
   const initialStates = {
     name: '',
     price: 1000
   }
 
-  //状態管理
-  const [name, setName] = useState(initialStates.name);
-  const [price, setPrice] = useState(initialStates.price);
+  //状態管理(propsを受け取っている)
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
 
   return (
     <div>
       <p>現在の{name}は、{price}円です。</p>
     </div>
   );
+}
+
+//propsの内容を定義
+App.defaultProps = {
+  name: '',
+  price: 1000
 }
