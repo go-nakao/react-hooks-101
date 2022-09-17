5ba5555586e083f84da4cfbc8d45aa9fa6b06dec
go-nakao <79854138+go-nakao@users.noreply.github.com>
2022-09-17 18:11:43 +0900

    金額リセットボタンの実装

--- src/App.js
+++ src/App.js
@@ -19,12 +19,18 @@ export const App = props => {
     setPrice((price) => price - 1);
   }
 
+  const resetPrice = () => {
+    setPrice((price) => price = 0);
+  }
+
   return (
     <div>
       <p>現在の{name}は、{price}円です。</p>
       <button onClick={incrementPrice}>金額設定+1</button>
       <br />
       <button onClick={decrementPrice}>金額設定-1</button>
+      <br />
+      <button onClick={resetPrice}>リセット</button>
     </div>
   );
 }
