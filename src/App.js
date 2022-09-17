bd49799a37f40d468cfc8b5c5ee1973a13896d7f
go-nakao <79854138+go-nakao@users.noreply.github.com>
2022-09-17 12:23:07 +0900

    現在の{name}は、{price}円です。

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const App = () => {
+  //初期値の設定
+  const initialStates = {
+    name: '',
+    price: 1000
+  }
+
+  //状態管理
+  const [name, setName] = useState(initialStates.name);
+  const [price, setPrice] = useState(initialStates.price);
+
   return (
     <div>
-      This is a templete for react.
+      <p>現在の{name}は、{price}円です。</p>
     </div>
   );
 }
